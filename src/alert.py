6b984ee6ab5a7d5fc796f88a7c97dbaf9bcef176
Horváth Balázs <tcgmilanofficial@gmail.com>
2022-03-28 01:17:29 +0200

    Teszteles 1.0

--- src/alert.py
+++ src/alert.py
@@ -8,8 +8,13 @@ class Alert:
 
     def init(self):
         self.warnings = [x for x in open("figyelmeztetesek.txt", "r")]
+        for x in self.engine.getProperty("voices"):
+            print(x)
         self.engine.setProperty("voice", "com.apple.speech.synthesis.voice.mariska")
 
     def read_warning(self):
         self.engine.say(random.choice(self.warnings))
         self.engine.runAndWait()
+
+a = Alert()
+a.init()
