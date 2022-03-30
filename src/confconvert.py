f6cad7ed5242ac383207e83b4e2940a428c1ba29
Horváth Balázs <tcgmilanofficial@gmail.com>
2022-03-30 13:18:17 +0200

    Config teszt

--- src/confconvert.py
+++ src/confconvert.py
@@ -9,13 +9,3 @@ def to_int(x : str):
         return int(x)
     except:
         return 0
-
-
-
-config = configparser.ConfigParser()
-config.read("./BEALLITASOK.cfg", encoding = "utf-8")
-
-test = config["BEALLITASOK"]["video_kimenet"]
-test2 = config["BEALLITASOK"]["figyelmeztetes_varakozas"]
-print(to_bool(test))
-print(to_int(test2))
