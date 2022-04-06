ddab42b5c454783c5fe1c71e38802ac34b847537
Horváth Balázs <tcgmilanofficial@gmail.com>
2022-04-06 12:08:31 +0200

    Teszt: 1.0.5

--- src/logger.py
+++ src/logger.py
@@ -18,7 +18,7 @@ class Logger:
         self.path = "/home/pi/mbot-mask-detection/"
         self.current_time = "[" + datetime.datetime.now().strftime("%Y/%m/%d - %H:%M:%S") + "]"
     def init(self):
-            open(os.path.join(self.path, self.current_time + ".txt"), "x", encoding = "utf-8")
+            open(os.path.join(self.path, self.current_time + ".txt"), "w+", encoding = "utf-8")
             self.log_file = open(os.path.join(self.path, self.current_time + ".txt"), "a", encoding = "utf-8")
     def wrtie_log(self, text : str):
         self.log_file.write(text + "\n")
