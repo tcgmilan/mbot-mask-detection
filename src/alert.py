3efc25810a4a24fb400877950cf03f27c0a8d967
Horváth Balázs <tcgmilanofficial@gmail.com>
2022-04-07 11:55:34 +0200

    Teszt: 1.0.6

--- src/alert.py
+++ src/alert.py
@@ -18,7 +18,7 @@ class Alert:
         self.engine = pyttsx3.init()
     def init(self):
         self.warnings = [x for x in open("/home/pi/mbot-mask-detection/figyelmeztetesek.txt", "r")]        
-        self.warnings = [x for x in open("/home/pi/mbot-mask-detection/dicseretek.txt", "r")]
+        self.awards = [x for x in open("/home/pi/mbot-mask-detection/dicseretek.txt", "r")]
         self.engine.setProperty("voice", "hungarian")
     def read_warning(self):
         self.engine.say(random.choice(self.warnings))
